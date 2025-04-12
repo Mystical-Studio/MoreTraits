82e81abf76f321f2b15277cb8fd8ecba44f9263f
Thomas Froud <thomasfroud10@gmail.com>
2025-04-12 12:52:51 +0100

    Social Skill is now reduced for the OptimistPlus

--- Source/MoreTraits/MoreTraits/Thoughts/ThoughtWorker_OptimistPlus.cs
+++ Source/MoreTraits/MoreTraits/Thoughts/ThoughtWorker_OptimistPlus.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
 
 namespace MoreTraits.Thoughts
@@ -15,9 +16,17 @@ namespace MoreTraits.Thoughts
             if (p.story.traits.HasTrait(TraitDef.Named("OptimistPlus")))
             {
                 Log.Message("OptimistPlus trait is active, triggering thought.");
+
+                var socialSkill = p.skills.GetSkill(SkillDefOf.Social);
+                Log.Message($"Social skill before adjustment: {p.skills.GetSkill(SkillDefOf.Social).Level}");
+                if (socialSkill != null)
+                {
+                    socialSkill.Level = Mathf.Max(0, socialSkill.Level - 1);
+                    Log.Message($"Social skill after adjustment: {p.skills.GetSkill(SkillDefOf.Social).Level}");
+                }
+
                 return ThoughtState.ActiveDefault;
             }
-
             return ThoughtState.Inactive;
         }
     }
