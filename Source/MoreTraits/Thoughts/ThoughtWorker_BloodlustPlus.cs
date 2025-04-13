c49a89ee55aefe7302b8d9e25cefd808b0c5e7e6
Thomas Froud <thomasfroud10@gmail.com>
2025-04-13 10:47:18 +0100

    Created reckless trait

---
+++ Source/MoreTraits/Thoughts/ThoughtWorker_BloodlustPlus.cs
@@ -0,0 +1,22 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace MoreTraits.Thoughts
+{
+    public class ThoughtWorker_BloodlustPlus : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            if (p.story.traits.HasTrait(TraitDef.Named("BloodlustPlus")))
+            {
+                return ThoughtState.ActiveDefault;
+            }
+            return ThoughtState.Inactive;
+        }
+    }
+}
